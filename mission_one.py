b4a5927027fa6b7912ac488d4bd1093ae7e3546f
kmortime <96500206+kmortime@users.noreply.github.com>
2022-10-12 09:01:42 -0400

    example of turning

--- mission_one.py
+++ mission_one.py
@@ -12,6 +12,32 @@ def mission_one(r):
     print("Running Mission 1")
     #Lydia and Madeleine 
     r.ev3.screen.draw_text(30, 60, "Mission 1")
+
+    ############################
+    # BEGIN: EXAMPLE FROM THE SPRING
+    ############################
+    # Create and define distance, speed, and turn variables
+    # to control how fast the robot moves, how sharp it turns, and how far it will go.
+    # Reset the robot distance to zero
+    speed = 200
+    turn = 20
+    distance = 600
+    r.robot.reset()
+
+    # While the robot.distance() is less than or equal to the variable 'distance' stay in the while loop
+    while (r.robot.distance() <= distance):
+        r.robot.drive(speed,turn)
+    
+    print("The robot has moved " + str(r.robot.distance()) + " mm")
+
+    # r.robot.drive() goes on forever, even after the while loop ends.
+    # You need to stop the robot after exiting the while loop
+    r.robot.stop()
+
+    ############################
+    # END: EXAMPLE FROM THE SPRING
+    ############################
+
     r.robot.straight(600)
     r.left_attachment_motor.run(200) 
     wait(1000)
